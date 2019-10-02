11c444bde2ef6ec9a84bd69e22bd51b4e1878c78
bgudenas <bgudena@g.clemson.edu>
2019-10-02 15:25:37 -0500

    Broke into two functions

--- Cell_Filter.R
+++ Cell_Filter.R
@@ -1,9 +1,8 @@
-#!/usr/bin/env Rscript
+
 source("./Seurat_Helpers/Basic_Helpers.R")
 
-Cell_Filter = function(mega, samps ){
-  ## Filter out weird cell populations
-
+Celltype_Assign = function(mega ) {
+  
 cluster.averages <- AverageExpression(object = mega, verbose = FALSE, use.scale = TRUE)
 clustmat =  cluster.averages$RNA
 GO  = readRDS("./Data/scRNA_celltypes.rds")
@@ -20,8 +19,8 @@ pcount = sum(grepl("UMAP_cellTypes", list.files("~/Proj/scPB/Figures/"))) + 1
 ggsave(p3, device = "pdf", filename = paste0("~/Proj/scPB/Figures/UMAP_cellTypes_", pcount, ".pdf"), width = 14, height = 10)
 
 drops = c("Reticulocytes", "Nuocytes", "Platelets","Erythroid−like and erythroid precursor cells","Basophils","Macrophages", 
-          "Oligodendrocyte progenitor cells", "Immature neurons", "Pyramidal cells", 
-          "Glutaminergic neurons", "GABAergic neurons", "Cajal-Retzius cells", "Choroid plexus cells", "Schwann cells", "Myeloid−derived suppressor cells")
+          "Oligodendrocyte progenitor cells","Ependymal cells", "Immature nerons", 
+          "Glutaminergic neurons", "GABAergic neurons", "Cajal-Retzius cells", "Choroid plexus cells", "Myeloid−derived suppressor cells")
 
 dropcells = names(mega@active.ident)[ (mega@active.ident %in% drops) ]
 dropcells = unlist(lapply(stringr::str_split(dropcells, "_"), "[[", 1))
@@ -30,52 +29,50 @@ mega@active.ident = clusters
 
 df = data.frame("Cells" = dropcells, "Timepoint" = names(dropcells) )
 write.table(df, "./Data/DropCells.csv", append = TRUE, sep = ",")
-dropcells = read.csv("./Data/DropCells.csv" )
-rm(mega)
 
-print(samps)
-sampIDs = c()
-for (i in 1:length(samps )) {
-   ID = unlist(lapply(stringr::str_split(dirname(samps[i]), "/"), "[[", 3 ))
-  print(ID)
-  ## Break on "-"; TP is which element has 3 chars
-  TP = unlist(stringr::str_split(ID, "-" ))[unlist(lapply(stringr::str_split(ID, "-" ), nchar)) == 3]
-  sampIDs = c(sampIDs, TP)
-  counts <- Read10X(data.dir = samps[i] )
-  
-  drops = dropcells$Cells[dropcells$Timepoint %in% TP ]
-  print(paste("Removed", sum( colnames(counts) %in% drops), "in", TP ))
+}
 
-  counts =counts[ , !(colnames(counts) %in% drops) ]
-  s1 = CreateSeuratObject(counts  =  counts, min.cells = 40, min.features = 500, project = TP )
+Reform_Seurat = function(samps ) {
+  sampIDs = c()
+  dropcells = read.csv("./Data/DropCells.csv", row.names=NULL)
   
-  s1[["percent.mt"]] <- PercentageFeatureSet(object = s1, pattern = "^mt-")    
-  s1$Timepoint = TP
-  #VlnPlot(object = s1, features = c("nFeature_RNA","nCount_RNA",  "percent.mt"), ncol = 3 , pt.size = .05, cols = c("dodgerblue" ) )
-  #ggsave(last_plot(), device = "pdf", filename = paste0("~/Proj/scPB/Figures/","QC_cells_", TP, ".pdf"), height = 7, width = 8)
-  # s1 = subset(x = s1, subset = nCount_RNA > 6000  &  nFeature_RNA > 2500 )
-  
-  if ( !exists("mega") ){
-    mega = s1
-  } else {
-    mega = merge( mega, y=s1, project = "Pineal_Atlas" )
+  for (i in 1:length(samps )) {
+    ID = unlist(lapply(stringr::str_split(dirname(samps[i]), "/"), "[[", 3 ))
+    print(ID)
+    ## Break on "-"; TP is which element has 3 chars
+    TP = unlist(stringr::str_split(ID, "-" ))[unlist(lapply(stringr::str_split(ID, "-" ), nchar)) == 3]
+    sampIDs = c(sampIDs, TP)
+    counts <- Read10X(data.dir = samps[i] )
+    
+    drops = dropcells$Cells[dropcells$Timepoint %in% TP ]
+    print(paste("Removed", sum( colnames(counts) %in% drops), "in", TP ))
+    
+    counts =counts[ , !(colnames(counts) %in% drops) ]
+    s1 = CreateSeuratObject(counts  =  counts, min.cells = 40, min.features = 500, project = TP )
+    rm(counts)
+    s1[["percent.mt"]] <- PercentageFeatureSet(object = s1, pattern = "^mt-")    
+    s1$Timepoint = TP
+    
+    if ( !exists("mega") ){
+      mega = s1
+    } else {
+      mega = merge( mega, y=s1, project = "Pineal_Atlas" )
+    }
   }
+  
+  mega = subset(x = mega, subset = nCount_RNA > 5000  &  nFeature_RNA > 1500 )
+  mega = subset(x = mega, subset = nCount_RNA < 70000 )
+  mega = subset(x = mega, subset = percent.mt < 12 )
+  mega = ProcSeurat(mega)
+  
+  ##Change to simple cap for mouse
+  s.genes = tolower(cc.genes$s.genes)
+  s.genes = unlist(lapply(s.genes, simpleCap ))
+  g2m.genes = tolower(cc.genes$g2m.genes)
+  g2m.genes = unlist(lapply(g2m.genes, simpleCap ))
+  mega <- CellCycleScoring(mega, s.features = s.genes, g2m.features = g2m.genes, set.ident = TRUE)
+  mega$CC.Difference <- mega$S.Score - mega$G2M.Score
+  mega = ProcSeurat(mega,  "CC.Difference")
+  
+  return(mega)
 }
-
-mega = subset(x = mega, subset = nCount_RNA > 5000  &  nFeature_RNA > 2000 )
-mega = subset(x = mega, subset = nCount_RNA < 80000 )
-mega = subset(x = mega, subset = percent.mt <= 10 )
-mega = ProcSeurat(mega)
-
-##Change to simple cap for mouse
-s.genes = tolower(cc.genes$s.genes)
-s.genes = unlist(lapply(s.genes, simpleCap ))
-g2m.genes = tolower(cc.genes$g2m.genes)
-g2m.genes = unlist(lapply(g2m.genes, simpleCap ))
-mega <- CellCycleScoring(mega, s.features = s.genes, g2m.features = g2m.genes, set.ident = TRUE)
-mega$CC.Difference <- mega$S.Score - mega$G2M.Score
-mega = ProcSeurat(mega,  "CC.Difference")
-
-return(mega)
-}
-
