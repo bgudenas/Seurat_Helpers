a5e715fd88da75664fb0a8b74a87b9295d983d34
bgudenas <bgudena@g.clemson.edu>
2020-10-12 18:10:35 -0500

    Updated DF sweep params

--- FindDoublets.R
+++ FindDoublets.R
@@ -1,13 +1,13 @@
 count_path = "2008335_E17_F2/outs/filtered_feature_bc_matrix"
 
-Doublets = function(count_path,
+Find_Doublets = function(count_path,
                     mt_filt = 10,
-                    nDims = 10 ){
+                    nDims = 15 ){
 library(Seurat)
 library(DoubletFinder)
   
 gene_counts = Read10X(count_path )
-so <- CreateSeuratObject(gene_counts, min.cells = 100)
+so <- CreateSeuratObject(gene_counts, min.cells = 50)
 
 
 if (sum(grepl("MT-", rownames(so))) == 0 ){
@@ -19,6 +19,8 @@ if (sum(grepl("MT-", rownames(so))) == 0 ){
 # Adaptive QC thresholds --------------------------------------------------
 qc.low_lib = scater::isOutlier(so$nCount_RNA, log = TRUE,  type="lower")
 qc.low_genes = scater::isOutlier(so$nFeature_RNA, log = TRUE,  type="lower")
+
+print(table(qc.low_lib, qc.low_genes))
 
 keep_cells = colnames(so)[!( qc.low_lib | qc.low_genes)]
 so = subset(so, cells = keep_cells  )
@@ -33,7 +35,7 @@ so <- FindNeighbors(object = so, reduction = "pca", dims = 1:nDims)
 so <- FindClusters(so, n.start =  100 , resolution = 0.5 )
 
 
-sweep.res.list_so <- paramSweep_v3(so, PCs = 1:10, sct = FALSE)
+sweep.res.list_so <- paramSweep_v3(so, PCs = 1:nDims, sct = FALSE)
 sweep.stats_so <- summarizeSweep(sweep.res.list_so, GT = FALSE)
 bcmvn_so <- find.pK(sweep.stats_so)
 
@@ -44,7 +46,7 @@ dub_rate = ncol(so)/1000 * 0.8 ## 0.8% doublets per 1k cells
 nExp_poi <- round(0.04*nrow(so@meta.data))  
 nExp_poi.adj <- round(nExp_poi*(1-homotypic.prop))
 
-so <- doubletFinder_v3(so, PCs = 1:10, pN = 0.25, pK = pK, nExp = nExp_poi, reuse.pANN = FALSE, sct = FALSE)
+so <- doubletFinder_v3(so, PCs = 1:nDims, pN = 0.25, pK = pK, nExp = nExp_poi, reuse.pANN = FALSE, sct = FALSE)
 DF_col =  which(grepl("DF.classifications", colnames(so@meta.data) ))
 
 dub_calls = so@meta.data[ ,DF_col]
