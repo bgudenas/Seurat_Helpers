1a90b8a32e209cbd0382b13f111b48797f67bd83
bgudenas <bgudena@g.clemson.edu>
2020-10-12 20:00:03 -0500

    rename mega to so

--- Cell_QC_Plots.R
+++ Cell_QC_Plots.R
@@ -1,25 +1,25 @@
 
 # QC_Plots
 
-Cell_QC_Plots = function( seurat_object, plotfile = NULL){
+Cell_QC_Plots = function( so, plotfile = NULL){
 library(Seurat)
   library(ggplot2)
 pdf(plotfile)
 par(mfrow=c(2,2))
 
-hist(mega$nCount_RNA, breaks = 100, col = "dodgerblue", main = "Count Depth", xlab = "") 
-abline(v = median(mega$nCount_RNA), col = "red", lwd = 2)
+hist(so$nCount_RNA, breaks = 100, col = "dodgerblue", main = "Count Depth", xlab = "") 
+abline(v = median(so$nCount_RNA), col = "red", lwd = 2)
 
-hist(mega$nFeature_RNA,breaks = 100, col = "dodgerblue", main = "# of Genes", xlab = "") 
-abline(v = median(mega$nFeature_RNA), col = "red", lwd = 2)
+hist(so$nFeature_RNA,breaks = 100, col = "dodgerblue", main = "# of Genes", xlab = "") 
+abline(v = median(so$nFeature_RNA), col = "red", lwd = 2)
 
-hist(mega$percent.mt, breaks = 100 , col = "dodgerblue", main = "% Mitochondria", xlab = "") 
-abline( v = median(mega$percent.mt), col = "red", lwd = 2 )
+hist(so$percent.mt, breaks = 100 , col = "dodgerblue", main = "% Mitochondria", xlab = "") 
+abline( v = median(so$percent.mt), col = "red", lwd = 2 )
 
 ## Plot Mitochondira by counts
-df = data.frame("Count" = mega$nCount_RNA,
-                "Genes" = mega$nFeature_RNA,
-                "Mitochondria" = mega$percent.mt)
+df = data.frame("Count" = so$nCount_RNA,
+                "Genes" = so$nFeature_RNA,
+                "Mitochondria" = so$percent.mt)
 
 g1 = ggplot(df, aes(x = Count, y = Genes) ) +
   geom_point(size = 1, alpha = 0.6, aes(col = Mitochondria )) +
