01b7524465e83ee02331d02dbed3966f59366ace
bgudenas <bgudena@g.clemson.edu>
2020-10-18 15:05:59 -0500

    Add Joined_QC plot

--- Cell_QC_Plots.R
+++ Cell_QC_Plots.R
@@ -30,3 +30,39 @@ g1 = ggplot(df, aes(x = Count, y = Genes) ) +
 print(g1)
 dev.off()
 }
+
+
+
+
+library(dplyr)
+library(ggplot2)
+
+Joined_QC = function(mega, QC_dir, plot_name) {
+  ## Plots QC metrics across samples to find problematic samples
+  library(ggplot2)
+  library(dplyr)
+  df = data.frame("Sample" = mega$Sample, "nCount_RNA" = log2(mega$nCount_RNA), "percent.mt" = mega$percent.mt )
+  ords = df %>% 
+    group_by(Sample) %>% 
+    summarise("Med" = median(nCount_RNA)) %>% 
+    arrange(desc(Med)) %>% 
+    .$Sample
+  df$Sample = factor(df$Sample, ordered = TRUE, levels = ords )
+  
+  g1 = ggplot(df, aes(x = Sample, y = nCount_RNA, group = Sample )) +
+    geom_violin( aes(fill = Sample ) ) +
+    geom_hline(yintercept =  median(df$nCount_RNA), linetype = "dashed", col = "black") + 
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.position = "none" ) + 
+    ylab("Log2_nCount_RNA") 
+  
+  g2 = ggplot(df, aes(x = Sample, y = percent.mt, group = Sample )) +
+    geom_violin( aes(fill = Sample ) ) +
+    geom_hline(yintercept =  median(df$percent.mt), linetype = "dashed", col = "black") + 
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.position = "none" ) + 
+    ylab("percent.mt") 
+  
+  cowplot::plot_grid(g1,g2, ncol= 1)
+  ggsave(last_plot(), filename = file.path(QC_dir, plot_name), device = "pdf")
+}
