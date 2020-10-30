1d7756743e360512bc68ee6eb97a663e8a37bf38
bgudenas <bgudena@g.clemson.edu>
2020-10-29 19:35:11 -0500

    Remove Auto Cell

--- Basic_Helpers.R
+++ Basic_Helpers.R
@@ -22,31 +22,6 @@ PlotSeurat = function(plot, name, path ){
 
 
 
-AutoCellType = function(mega, GO, QC_dir ){
-  ## GO is a list of lists (celltypes and their marker genes)
-  cluster.averages <- AverageExpression(object = mega, verbose = FALSE, use.scale = TRUE)
-  clustmat =  cluster.averages$RNA
-  
-  resSS = GSVA::gsva(as.matrix(clustmat), GO, method="ssgsea", ssgsea.norm = FALSE, min.sz= 3, verbose = FALSE )
-  df = as.data.frame(matrix(nrow= ncol(clustmat), ncol = 3 ) )
-  colnames(df) = c("Cluster", "SSGSEA","Score" )
-  rownames(df) = colnames(clustmat)
-  
-  vals = c()
-  for (i in 1:ncol(resSS)){
-    vals =c(vals, rownames(resSS)[which.max(resSS[ ,i])] )
-  }
-  
-  df$SSGSEA = vals
-  df$Score = apply(resSS, 2, max)
-  df$Cluster = colnames(clustmat)
-  mega$auto_celltype = df$SSGSEA[match(mega$seurat_clusters, df$Cluster)]
-  g1 = DimPlot(mega, group.by= "auto_celltype", label = TRUE)
-  ggplot2::ggsave(g1, device = "png", filename=file.path(QC_dir, "Auto_celltype.png"), width = 10, height = 7)
-  
-  return(mega)
-}
-
 
 ProcSeurat = function(mega, vars.to.regress=NULL, nDim = 20, nGenes = 2000 ) {
   
